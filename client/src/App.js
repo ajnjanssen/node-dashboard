b2fcaced75d703963ce47415e6d203d712fa19a2
alex <ajn.janssen@gmail.com>
2022-12-04 15:12:18 +0100

    added private routes for authenticated users

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { ColorModeContext, useMode } from './theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Router } from 'react-router-dom'
 import React, { useState, useEffect } from 'react'
 import Topbar from './scenes/global/Topbar'
 import Sidebar from './scenes/global/SidebarContainer'
@@ -16,37 +16,37 @@ import Pie from './scenes/pie/'
 import FAQ from './scenes/faq/'
 import Geography from './scenes/geography/'
 import Calendar from './scenes/calendar/'
+import PrivateRoutes from './utils/PrivateRoutes'
 
 function App() {
     return (
-        //    <ColorModeContext.Provider>
-        //  <ColorModeContext.Provider value={colorMode}>
-        //    <ThemeProvider>
-        //    <ThemeProvider theme={theme}>
-        // <CssBaseline />
         <div className="app font-poppins">
             <Sidebar />
             <main className="content">
                 <Topbar />
                 <Routes>
-                    <Route exact path="/login" element={<Login />} />
-                    <Route exact path="/" element={<Dashboard />} />
-                    <Route exact path="/team" element={<Team />} />
-                    <Route exact path="/invoices" element={<Invoices />} />
-                    <Route exact path="/contacts" element={<Contacts />} />
-                    <Route exact path="/form" element={<Form />} />
-                    <Route exact path="/faq" element={<FAQ />} />
-                    <Route exact path="/calendar" element={<Calendar />} />
-                    {/* <Route exact path="/bar" element={<Bar />} />
-              <Route exact path="/line" element={<Line />} />
-              <Route exact path="/pie" element={<Pie />} />
-              <Route exact path="/geography" element={<Geography />} /> */}
+                    <Route element={<PrivateRoutes />}>
+                        <Route exact path="/" element={<Dashboard />} />
+                        <Route exact path="/team" element={<Team />} />
+                        <Route exact path="/invoices" element={<Invoices />} />
+                        <Route exact path="/contacts" element={<Contacts />} />
+                        <Route exact path="/form" element={<Form />} />
+                        <Route exact path="/faq" element={<FAQ />} />
+                        <Route exact path="/calendar" element={<Calendar />} />
+                    </Route>
+                    <Route element={<Login />} path="/login" />
+
+                    {/* <Route path="/" element={<Dashboard />} /> */}
                 </Routes>
             </main>
         </div>
-        //    </ThemeProvider>
-        // </ColorModeContext.Provider>
     )
 }
 
 export default App
+
+//    <ColorModeContext.Provider>
+//  <ColorModeContext.Provider value={colorMode}>
+//    <ThemeProvider>
+//    <ThemeProvider theme={theme}>
+// <CssBaseline />
